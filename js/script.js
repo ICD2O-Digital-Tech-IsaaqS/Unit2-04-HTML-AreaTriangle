03a022b62fb4bdcb2f218ed5d352657a4aed3e81
Isaaq Simon <IsaaqS>
2025-03-27 14:33:00 +0000

    Triangle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area of a triangle.
+ */
+function calculate () {
+  // input
+  let length = parseInt(document.getElementById('length-of-rectangle').value);
+  let width = parseInt(document.getElementById('width-of-rectangle').value);
+
+  // process
+  let area = length * width;
+  let perimeter = 2 * (length + width);
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
